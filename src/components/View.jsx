98172ddb15de44b353c3af1378ef5567030d97be
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-24 00:17:43 +0600

    Add raw body option

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 const View = () => {
   const [link, setLink] = useState("http://localhost:4000");
   const [result, setResult] = useState("");
+  const [reqBody, setReqBody] = useState("");
+  const [bodyIsValidJson, setBodyIsValidJson] = useState(false);
+  const [isSendBtnEnabled, setIsSendBtnEnabled] = useState(true);
   const [selectedMethod, setSelectedMethod] = useState("GET");
 
   const methods = ["GET", "POST", "PUT", "DELETE"];
@@ -12,8 +15,27 @@ const View = () => {
     setLink(event.target.value);
   };
 
-  const handleMethodChane = (event) => {
+  const handleMethodChange = (event) => {
+    const method = event.target.value;
+    if (method == "POST" || method == "PUT") {
+      setIsSendBtnEnabled(bodyIsValidJson);
+    }
     setSelectedMethod(event.target.value);
+  };
+
+  const IsValidJson = (str) => {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleBodyInput = (event) => {
+    setBodyIsValidJson(IsValidJson(event.target.value));
+    setIsSendBtnEnabled(IsValidJson(event.target.value));
+    setReqBody(event.target.value);
   };
 
   const handleSendClick = async () => {
@@ -28,9 +50,21 @@ const View = () => {
 
     try {
       let response = null;
+      console.log(selectedMethod);
       switch (selectedMethod) {
         case "GET":
           response = await service.get();
+          break;
+        case "POST":
+          console.log("here");
+          response = await service.post(null, JSON.parse(reqBody));
+          break;
+        case "PUT":
+          response = await service.put(null, JSON.parse(reqBody));
+          break;
+        case "DELETE":
+          response = await service.delete();
+          break;
       }
       setResult(response.data);
     } catch (error) {
@@ -41,7 +75,7 @@ const View = () => {
 
   return (
     <div>
-      <select value={selectedMethod} onChange={handleMethodChane}>
+      <select value={selectedMethod} onChange={handleMethodChange}>
         {methods.map((method) => (
           <option key={method} value={method}>
             {method}
@@ -49,7 +83,11 @@ const View = () => {
         ))}
       </select>
       <input value={link} onChange={handleLinkInput} type="text" />
-      <button onClick={handleSendClick}>Send</button>
+      <button disabled={!isSendBtnEnabled} onClick={handleSendClick}>
+        Send
+      </button>
+      <br />
+      <textarea value={reqBody} onChange={handleBodyInput}></textarea>
       <br />
       <div>
         <pre>{result != "" ? JSON.stringify(result, null, 2) : null}</pre>
